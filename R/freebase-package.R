9742aaa4be13d3c894b0960aaf15c25d34178f32
boB Rudis <bob@rud.is>
2018-05-28 13:10:06 -0400

    b/c I cannot type today

--- R/freebase-package.R
+++ R/freebase-package.R
@@ -6,7 +6,7 @@
 #' 'tidyverse' functions that are mostly equivalent (some are more complete
 #' than others). The base R counterpart functions will likely be slower than
 #' the 'tidyverse' equivalents but using them will decrease 'Imports'
-#' dependencies. Use delibrately and with caution.
+#' dependencies.\cr\crUse deliberately and with caution.
 #'
 #' @md
 #' @name freebase
